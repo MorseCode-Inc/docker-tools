563d279c9d4c745f18991c9ec554d05803a740bb
Brad <brad@morsecode-inc.com>
2016-02-14 01:09:34 -0700

    safety will prevent install if not root

--- bin/install-otto.sh
+++ bin/install-otto.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-VER="0.2.0"
+OTTO_VER="0.2.0"
+VAGRANT_VER="1.8.1_x86_64"
 
 INST_DIR="$1"
+USR_BIN="/usr/bin"
 
 if [ ! -d "$INST_DIR" ]
 then
@@ -15,14 +17,22 @@ then
 	exit 1
 fi
 
+if [ ! -w "$USR_BIN" ]
+then
+	echo "Install directory $USR_BIN is not writeable, use sudo?"
+	exit 1
+fi
+
 cd "$INST_DIR"
-wget -O "otto-${VER}.zip" "https://releases.hashicorp.com/otto/${VER}/otto_${VER}_linux_amd64.zip"
-unzip "otto-${VER}.zip"
-sudo mv otto /usr/bin
+wget -O "otto-${OTTO_VER}.zip" "https://releases.hashicorp.com/otto/${OTTO_VER}/otto_${OTTO_VER}_linux_amd64.zip"
+unzip "otto-${OTTO_VER}.zip"
+mv otto "$USR_BIN"/
+
+wget -O vagrant_${VAGRANT_VER}.deb https://releases.hashicorp.com/vagrant/1.8.1/vagrant_${VAGRANT_VER}.deb
+apt install -y vagrant_${VAGRANT_VER}.deb
 
 echo
 echo "otto"
 otto
-
 
 exit
