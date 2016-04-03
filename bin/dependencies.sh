1737ac2c16bd7aae3f627008b45637648eee4b28
Brad <brad@morsecode-inc.com>
2016-04-02 21:18:17 -0600

    added sudo to dependency script

--- bin/dependencies.sh
+++ bin/dependencies.sh
@@ -3,8 +3,8 @@
 OTTO_VER="0.2.0"
 VAGRANT_VER="1.8.1_x86_64"
 
-apte installinux-headers-$(uname -r)
-apt install virtualbox
+sudo apte installinux-headers-$(uname -r)
+sudo apt install virtualbox
 sudo dpkg-reconfigure virtualbox-dkms
 
 
